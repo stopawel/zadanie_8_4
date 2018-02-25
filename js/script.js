ebf0e685d446b015d37f5adc257250bfa2ed6409
Paweł Stolecki <noigell@gmail.com>
2018-02-25 21:55:23 +0100

    modified js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // script.js
 
+//alert("Hello! I am an alert box!!");
+
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 consol.log('Hello, ' + name);
+
+/*kolejny komentarz
+tym razem
+blokowy*/
+
+// komentarz liniowy
